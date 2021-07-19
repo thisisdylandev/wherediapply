e1fb9ab18bc2ef2e3e5de88732714c9ce82ec1ed
thisisdylandev <dylan@ctrlaltdylan.com>
2021-07-19 10:34:23 -0500

    test: add basic user test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
+require "faker"
+
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Books::Lovecraft.fhtagn }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    new_user = FactoryBot.build(:user)
+    expect(new_user).to be_valid
+  end
+  it "is not valid without an email" do
+    no_email = User.new(email: nil)
+    expect(no_email).to_not be_valid
+  end
+  it "is not valid without a password" do
+    no_password = User.new(password: nil)
+    expect(no_password).to_not be_valid
+  end
 end
